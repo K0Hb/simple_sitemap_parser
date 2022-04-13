5951abcad30f866e73bfca82e4bd0e14b3b95f07
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-04-13 17:22:33 +0300

    Доработал парсерс

--- parser_v2.py
+++ parser_v2.py
@@ -20,30 +20,23 @@ def valid_url(url):
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
 
-# def get_html(url):
-#     try:
-#         html = requests.get(url, timeout=5)
-#     except Exception:
-#         return None
-#     if html.status_code != 200:
-#         return None
-#     return html
 
 async def get_html(session, url):
-        async with session.get(url) as resp:
+        async with session.get(url, ssl=False) as resp:
             try:
-                assert resp.status == 200
-            except Exception as e:
-                print(f'Error \n {e}')
+                status = resp.status
+                assert status == 200
+            except Exception:
+                print(f'Статус {status}, URL {url}')
                 return None
             respons = await resp.text()
-            print(resp.status, url)
+            print(f'Статус {status}, URL {url}')
             BUFFER.append((url, respons))
             return respons
 
 
-async def run_get_loop_html(url_list):
-    async with aiohttp.ClientSession() as session:
+async def create_loop_and_session(url_list):
+    async with aiohttp.ClientSession(connector = aiohttp.TCPConnector(verify_ssl=False)) as session:
         tasks = []
         for url in url_list:
             task = asyncio.create_task(get_html(session, url))
@@ -92,28 +85,23 @@ def parse_links(url_html):
         ALL_URLS.add(href)
     if len(urls) == 0:
         # страница без ссылок
-        return None
+        return []
     # ALL_COUNT += count
+    print(f"Парсер отработал , найдено {len(urls)} внутренних ссылок.")
     return iter(urls)
 
 def lol(queue_urls):
-    asyncio.run(run_get_loop_html(queue_urls))
+    asyncio.run(create_loop_and_session(queue_urls))
 
 def deep_crawl_website(base_url):
-    queue_urls = []
-    # for iter in queue_urls:
-    #     if iter is not None:
-    #         asyncio.run(run_get_loop_html(iter))
-    asyncio.run(run_get_loop_html([base_url,]))
+    iter_urls = [base_url,]   
     while True:
-        new_urls = parse_links(BUFFER.pop())
-        if new_urls is not None:
-            queue_urls.append(iter(new_urls))
-        try:
-            lol(queue_urls.pop())
-            print(f"sum packegs {len(queue_urls)}")
-        except IndexError:
-            continue
-            
-        
-deep_crawl_website('http://google.com/')
+        lol(iter_urls)
+        if len(BUFFER) > 0:
+            html = BUFFER.pop()
+            iter_urls = parse_links(html)
+        else:
+            print("Exit")
+            return 
+
+deep_crawl_website('https://stackoverflow.com')
